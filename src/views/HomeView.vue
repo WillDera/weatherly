0358d324451318cd43f3ea265b3a1375c630ca51
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-02-16 23:04:03 +0100

    UI is a hassle

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,6 +32,49 @@
       </div>
     </div>
   </div>
+
+
+  <section class="vh-100" style="background-color: #4B515D;">
+  <div class="container py-5 h-100">
+
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-8 col-lg-6 col-xl-4">
+
+        <div class="card" style="color: #4B515D; border-radius: 35px;">
+          <div class="card-body p-4">
+            <div class="d-flex input-group-append m-3">
+            <input v-model="location" type="text" class="form-control" placeholder="City Name">
+              <button @click="updateLocation" class="btn btn-outline-secondary" type="button" id="button-addon2">Retrieve</button>
+            </div>
+            <div class="d-flex">
+              <h6 class="flex-grow-1">Warsaw</h6>
+              <h6>15:07</h6>
+            </div>
+
+            <div class="d-flex flex-column text-center mt-5 mb-4">
+              <h6 class="display-4 mb-0 font-weight-bold" style="color: #1C2331;"> 13°C </h6>
+              <span class="small" style="color: #868B94">Stormy</span>
+            </div>
+
+            <div class="d-flex align-items-center">
+              <div class="flex-grow-1" style="font-size: 1rem;">
+                <div><i class="fas fa-wind fa-fw" style="color: #868B94;"></i> <span class="ms-1"> 40 km/h </span></div>
+                <div><i class="fas fa-tint fa-fw" style="color: #868B94;"></i> <span class="ms-1"> 84% </span></div>
+                <div><i class="fas fa-sun fa-fw" style="color: #868B94;"></i> <span class="ms-1"> 0.2h </span></div>
+              </div>
+              <div>
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/ilu1.webp" width="100px">
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</section>
 </template>
 
 <script>
@@ -118,25 +161,5 @@ export default {
 
 
 <style>
-.card-header {
-  display: inline-flex;
-  text-align: center;
-}
 
-.card-header2 {
-  display: inline-flex;
-  text-align: center;
-  background-color: rgba(0, 12, 121, 0.61);
-  padding: 7px;
-}
-
-.card-body {
-  display: inline-flex;
-  text-align: center;
-  background-color: rgba(51, 5, 177, 0.61);
-}
-
-.col > img {
-  height: 50px;
-}
 </style>
